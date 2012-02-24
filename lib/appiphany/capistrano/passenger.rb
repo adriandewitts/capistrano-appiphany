a52331743114ed2630fe8cab06ba35c8d785a4f5
ivanvr <github@ivanyvenian.com>
2012-02-24 06:19:47 -0600

    Compile passenger

--- lib/appiphany/capistrano/passenger.rb
+++ lib/appiphany/capistrano/passenger.rb
@@ -5,10 +5,22 @@ configuration = Capistrano::Configuration.respond_to?(:instance) ?
   Capistrano.configuration(:must_exist)
 
 configuration.load do
+  namespace :passenger do
+    desc 'Trigger compilation'
+    task :compile do
+      run "cd #{current_path} && bundle exec passenger status"
+    end
+  end
+
   namespace :deploy do
     desc 'Restarting Passenger with restart.txt'
-    task :restart, :roles => :app, :except => { :no_release => true } do
+    task :restart do
       run "touch #{current_path}/tmp/restart.txt"
+    end
+
+    after 'deploy' do
+      releases = capture("ls -l #{release_path}..").split("\n").size - 1
+      passenger.compile if releases == 1
     end
 
     [ :start, :stop ].each do |t|
